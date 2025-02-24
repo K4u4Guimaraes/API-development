90caaf8a6ef02ad644fb3886a390d2447db60a39
K4u4Guimaraes <kauaguimaraes0001@gmail.com>
2025-02-24 15:21:30 -0300

    testing directory

--- src/main/java/me/dev/API_development/ApiDevelopmentApplication.java
+++ src/main/java/me/dev/API_development/ApiDevelopmentApplication.java
@@ -2,10 +2,15 @@ package me.dev.API_development;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class ApiDevelopmentApplication {
 
+	@RequestMapping("/")
+	String home(){
+		return "hello world!, testing many things";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApiDevelopmentApplication.class, args);
 	}
