197212c97d041ab64c2aa6820b5769fcba20075d
K4u4Guimaraes <kauaguimaraes0001@gmail.com>
2025-02-24 15:58:04 -0300

    bugfix CORS

--- src/main/java/me/dev/API_development/ApiDevelopmentApplication.java
+++ src/main/java/me/dev/API_development/ApiDevelopmentApplication.java
@@ -1,5 +1,7 @@
 package me.dev.API_development;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@OpenAPIDefinition(servers = {@Server(url = "/",description = "Default Server URL")})
 @SpringBootApplication
 public class ApiDevelopmentApplication {
 
