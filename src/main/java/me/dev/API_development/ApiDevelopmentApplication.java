527dd63962b8878013aacc4d81abb64158eedf41
K4u4Guimaraes <kauaguimaraes0001@gmail.com>
2025-02-24 15:41:17 -0300

    hotfix directories

--- src/main/java/me/dev/API_development/ApiDevelopmentApplication.java
+++ src/main/java/me/dev/API_development/ApiDevelopmentApplication.java
@@ -2,6 +2,7 @@ package me.dev.API_development;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiDevelopmentApplication {
 
 	@RequestMapping("/")
-	String home(){
-		return "hello world!, testing many things";
+	public ResponseEntity<Void> home() {
+		return ResponseEntity.status(302) // Código de redirecionamento
+				.header("Location", "/swagger-ui/index.html")
+				.build();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApiDevelopmentApplication.class, args);
